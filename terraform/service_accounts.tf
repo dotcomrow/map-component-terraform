d4888623e87551069736a336799ee22144aa479c
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-08-16 15:41:29 -0400

    adding role
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/service_accounts.tf
+++ terraform/service_accounts.tf
@@ -2,3 +2,11 @@ resource "google_service_account" "data-layer-bigquery" {
   account_id   = "data-layer-bigquery"
   display_name = "data-layer-bigquery"
 }
+
+resource "google_project_iam_binding" "data-layer-bigquery" {
+  project = var.project
+  role    = "roles/bigquery.editor"
+  members = [
+    "serviceAccount:${google_service_account.data-layer-bigquery.email}"
+  ]
+}
