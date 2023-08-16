cd64dfaad60c7ee87cf61c4238b747f45285f595
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-08-16 14:56:30 -0400

    name fix
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/service_accounts.tf
+++ terraform/service_accounts.tf
@@ -1,4 +1,4 @@
-resource "google_service_account" "data_layer_bigquery" {
-  account_id   = "data_layer_bigquery"
-  display_name = "data_layer_bigquery"
+resource "google_service_account" "data-layer-bigquery" {
+  account_id   = "data-layer-bigquery"
+  display_name = "data-layer-bigquery"
 }
