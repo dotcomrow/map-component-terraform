cb565710d1473f4c25d0a140b419bbc190ee2842
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-08-18 17:28:03 -0400

    adding ol svc acct
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/service_accounts.tf
+++ terraform/service_accounts.tf
@@ -10,3 +10,16 @@ resource "google_project_iam_binding" "data-layer-bigquery" {
     "serviceAccount:${google_service_account.data-layer-bigquery.email}"
   ]
 }
+
+resource "google_service_account" "ol-layer" {
+  account_id   = "ol-layer"
+  display_name = "dol-layer"
+}
+
+# resource "google_project_iam_binding" "ol-layer" {
+#   project = var.project
+#   role    = "roles/bigquery.dataEditor"
+#   members = [
+#     "serviceAccount:${google_service_account.ol-layer.email}"
+#   ]
+# }
