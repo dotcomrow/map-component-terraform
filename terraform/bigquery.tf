21e1f50831d9b0ab99641b570a993ed261aab64e
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-08-20 13:36:57 -0400

    changing column type
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/bigquery.tf
+++ terraform/bigquery.tf
@@ -25,7 +25,7 @@ resource "google_bigquery_table" "map_component_poi_data" {
   },
   {
     "name": "DATA",
-    "type": "JSON",
+    "type": "RECORD",
     "mode": "REQUIRED",
     "description": "Data associated with POI"
   },
